7c9b4a826f8873d188edef14af2ce587e5df8249
Daniel Ao <danielao9570@gmail.com>
2024-06-01 13:58:34 -0700

    Add txt file parsing method into main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Path to the file
+        String filePath = "seqdump.txt";
+        List<String> lines = new ArrayList<>();
+
+        // Create a BufferedReader in a try-with-resources statement
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            StringBuilder toBeBuilt = new StringBuilder();
+            //
+            while ((line = br.readLine()) != null) {
+                if (line.charAt(0) == '>'){
+                    lines.add(toBeBuilt.toString());
+                    toBeBuilt.delete(0, toBeBuilt.length());
+                } else {
+                    toBeBuilt.append(line);
+                }
+            }
+        } catch (IOException e) {
+            // Handle the exception
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+
+        /*for(String c : lines){
+            System.out.println(c);
+            System.out.println( '\n');
+        }*/
     }
 }
